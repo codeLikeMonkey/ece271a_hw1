4ce2a0451e72281e4863963fac949d4f0aaf13c9
Tony <yhtcoding@gmail.com>
2017-10-09 12:12:02 -0700

    change ceil(.) to fix(.)

--- im2block.m
+++ im2block.m
@@ -9,8 +9,9 @@ function blocks = im2block(img,window)
 [m, n] = size(img);
 blocks = {};
 % x for x axis, y  for  y axis
-for y = 1 : ceil(m / window(1))
-    for x = 1 : ceil(n / window(2))
+for y = 1 : fix(m / window(1))
+    for x = 1 : fix(n / window(2))
         blocks{x,y} = imcrop(img,[(x - 1) * window(1) + 1, (y - 1) * window(2) + 1 , window(1) - 1,window(2) - 1]);
+        %block{x,y} = img((x - 1) * window(1) + 1 : x * window(1) , (y - 1) * window(2) + 1 : y * window(2));
     end
 end
