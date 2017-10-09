7ebb12084eca3edeb4ee353774dc73a3c976ff62
Tony <yhtcoding@gmail.com>
2017-10-09 13:13:50 -0700

    change vector parameters to mat parameters in find2ndx

--- find2ndX.m
+++ find2ndX.m
@@ -1,8 +1,14 @@
 %find position of 2nd largest value
-function pos = find2ndX(vec)
-abs_vec = abs(vec);
-sort_vec = sort(abs_vec,'descend');
-pos = find(abs_vec == sort_vec(2));
+function X = find2ndX(mat)
+X = [];
+for i = 1 : size(mat,1)
+    vec = mat(i,:);
+    abs_vec = abs(vec);
+    sort_vec = sort(abs_vec,'descend');
+    pos = find(abs_vec == sort_vec(2));
+    X = [X ; pos];
+end
+
 
 
 
