0a9ba4ad66b4a3af98aef20aeb6d0018796d201b
Tony <yhtcoding@gmail.com>
2017-10-09 02:18:42 -0700

    zigzag

---
+++ zigzag.m
@@ -0,0 +1,30 @@
+
+%convert img to zigzag vector
+function vec = zigzag(img)
+pattern = [0   1   5   6  14  15  27  28
+
+2   4   7  13  16  26  29  42
+
+3   8  12  17  25  30  41  43
+
+9  11  18  24  31  40  44  53
+
+10  19  23  32  39  45  52  54
+
+20  22  33  38  46  51  55  60
+
+21  34  37  47  50  56  59  61
+
+35  36  48  49  57  58  62  63
+];
+
+
+vec = zeros(1,64);
+for i = 1 : 8
+    for j = 1 : 8
+        vec(pattern(i,j) + 1) = img(i,j);
+    end
+end
+
+
+
