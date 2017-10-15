fa838f6f3922b0956aa04e073dbaccd406510cf9
Tony <yhtcoding@gmail.com>
2017-10-14 17:39:25 -0700

    add something required which gives information

--- cheetah.m
+++ cheetah.m
@@ -23,15 +23,33 @@ x_range = 64;
 
 X = find2ndX(zigzag_data);
 
+
+%%
+% Compute prior pobability
 hist_fg = hist(find2ndX(TrainsampleDCT_FG),1:x_range);
 hist_bg = hist(find2ndX(TrainsampleDCT_BG),1:x_range);
-
-P_fg = size(TrainsampleDCT_FG,1)/(size(TrainsampleDCT_BG,1) + size(TrainsampleDCT_FG,1));
+%prblem a
+P_fg = size(TrainsampleDCT_FG,1)/(size(TrainsampleDCT_BG,1) + size(TrainsampleDCT_FG,1))
 P_bg = 1 - P_fg;
 
 p_X = hist(X,1:x_range) / size(X,1);
-
+%%
+% probelm b
 p_x_given_fg = hist_fg / sum(hist_fg);
+p_x_given_bg = hist_bg / sum(hist_bg);
+%plot them
+figure
+bar(p_x_given_fg);
+xlabel('Index x');
+ylabel('Probability');
+title('Index histogram of Px|y(x|cheetah)','FontWeight','bold')
+
+figure
+bar(p_x_given_bg,'r');
+xlabel('Index x');
+ylabel('Probability');
+title('Index histogram of Px|y(x|grass)','FontWeight','bold')
+%%
 
 p_fg_given_x = [];
 for x = 1 : x_range
@@ -41,9 +59,17 @@ for x = 1 : x_range
         p_fg_given_x = [p_fg_given_x,1];
     end
 end
+%
+Y = p_fg_given_x;
+% figure
+% imagesc(Y)
+% colormap(gray(255))
+%title('c : state of variable Y');
+
+%%
 %finish mask
 fg_mask = p_fg_given_x >= 0.5;
-
+%imagesc(fg_mask)
 %mapping mask
 fg_result = zeros(size(X,1),1);
 for i = 1 : size(fg_mask,2)
@@ -60,12 +86,17 @@ for j = 1 + n / 2 :1:size(img,2) - n /2
     end
 end
 
-imshow(new_img);
+figure
+%imshow(new_img);
+imagesc(new_img)
+colormap(gray(255))
+title('The estimate image');
 mask_img = imread('cheetah_mask.bmp') > 0;
 cheetah_mask = mask_img(1:size(new_img,1),1:size(new_img,2));
-figure 
-imshow(cheetah_mask)
+%figure 
+%imshow(cheetah_mask)
 error_rate = sum(sum(abs(cheetah_mask - new_img))) / (size(new_img,1) * size(new_img,2)) * 100
+
 
 
 
